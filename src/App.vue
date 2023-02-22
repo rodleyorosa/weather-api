fe12cbc3174392b5986c814aa27210ee1cff7d10
rodleyorosa <rodleyorosa@gmail.com>
2023-02-22 10:12:08 +0100

    fixed weather icon

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,14 @@
         </svg>
       </button>
     </div>
+    <p v-if="isErrorVisible"
+    class="text-red-500">City not found </p>
     <div class="my-4 space-y-4">
       <h1 class="text-3xl font-bold">Weather in {{ city }}</h1>
       <h2 class="text-4xl font-semibold">{{ temp }}°C</h2>
       <div>
-        <p>{{ weather }}</p>
+        <p v-if="weatherIcon"
+        class="flex items-center"><img :src="weatherIcon" alt="weather-icon"> {{ weather }}</p>
         <p><span class="font-semibold">Humidity:</span> {{ humidity }}%</p>
         <p><span class="font-semibold">Wind:</span> {{ wind }}km/h</p>
       </div>
@@ -36,21 +39,35 @@ const apiKey = ref('5fa61531f96af9f4761f5cc669bbf7ad')
 
 const cityInput = ref(null)
 const bg = ref('https://source.unsplash.com/1600x900/?')
+const data = ref(null)
 const city = ref(null)
 const temp = ref(null)
 const weather = ref(null)
+const weatherIcon = ref(null)
 const humidity = ref(null)
 const wind = ref(null)
+const isErrorVisible = ref(false)
 
 async function getCity(city) {
-  let api = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey.value)
-  let data = api.data
-  this.city = data.name
-  this.temp = (data.main.temp - 273,15).toFixed(2)
-  this.weather = capitalize(data.weather[0].description)
-  this.humidity = data.main.humidity
-  this.wind = data.wind.speed
-
+  try {
+    const api = await axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey.value);
+    let data = api.data
+    this.data = api.data
+    this.city = data.name
+    this.temp = (data.main.temp - 273.15).toFixed(2)
+    this.weather = capitalize(data.weather[0].description)
+    this.weatherIcon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+    this.humidity = data.main.humidity 
+    this.wind = data.wind.speed
+    this.isErrorVisible = false
+  } catch (error) {
+    if(error.response.status === 404) {
+      this.isErrorVisible = true
+      console.clear()
+    } else {
+      console.error(error)
+    }
+  }
 }
 
 </script>
